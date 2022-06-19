da514a8b7821fc2dc1a939116b2c7858a9db09ae
oibene <alvespbene@gmail.com>
2022-06-19 16:07:39 -0300

    aaa

---
+++ main.js
@@ -0,0 +1,18 @@
+/** SCROOLLREVEAL mostra os elementos apartir do rolamento da pag **/
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+  })
+  
+  scrollReveal.reveal(
+    ` #home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links,
+    footer .brand, footer .social
+    `,
+    { interval: 100 }
+  )
